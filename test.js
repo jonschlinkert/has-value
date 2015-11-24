2ba8c526753cacc209767e0c6371003361ceb5ea
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-23 21:21:09 -0500

    use assert

--- test.js
+++ test.js
@@ -7,8 +7,8 @@
 
 'use strict';
 
-/* deps:mocha */
-var should = require('should');
+require('mocha');
+var assert = require('assert');
 var hasValue = require('./');
 
 function isEmpty(o, isZero) {
@@ -21,168 +21,168 @@ var a = {b: {c: o}};
 
 describe('object properties', function () {
   it('should work for nulls', function () {
-    hasValue(o, 'a').should.be.false;
-    hasValue(o, 'b').should.be.false;
+    assert(!hasValue(o, 'a'));
+    assert(!hasValue(o, 'b'));
   });
 
   it('should work for booleans', function () {
-    hasValue(o, 'c').should.be.true;
-    hasValue(o, 'd').should.be.true;
+    assert(hasValue(o, 'c'));
+    assert(hasValue(o, 'd'));
   });
 
   it('shold work for strings', function () {
-    hasValue(o, 'e').should.be.false;
-    hasValue(o, 'f').should.be.true;
+    assert(!hasValue(o, 'e'));
+    assert(hasValue(o, 'f'));
   });
 
   it('should work for numbers', function () {
-    hasValue(o, 'g').should.be.true;
-    hasValue(o, 'h').should.be.true;
+    assert(hasValue(o, 'g'));
+    assert(hasValue(o, 'h'));
   });
 
   it('should treat zero as null when `noZero` is set', function () {
-    hasValue(o, 'i', true).should.be.false;
+    assert(!hasValue(o, 'i', true));
   });
 
   it('should work for objects', function () {
-    hasValue(o, 'j').should.be.false;
-    hasValue(o, 'k').should.be.true;
+    assert(!hasValue(o, 'j'));
+    assert(hasValue(o, 'k'));
   });
 
   it('should work for arrays', function () {
-    hasValue(o, 'l').should.be.false;
-    hasValue(o, 'm').should.be.true;
+    assert(!hasValue(o, 'l'));
+    assert(hasValue(o, 'm'));
   });
 
   it('should work for functions', function () {
-    hasValue(o, 'n').should.be.true;
-    hasValue(o, 'o').should.be.false;
-    hasValue(o, 'o').should.not.be.true;
+    assert(hasValue(o, 'n'));
+    assert(!hasValue(o, 'o'));
+    assert(!hasValue(o, 'o'));
   });
 });
 
 describe('nested object properties', function () {
   it('should work for nulls', function () {
-    hasValue(a, 'b.c.a').should.be.false;
-    hasValue(a, 'b.c.b').should.be.false;
+    assert(!hasValue(a, 'b.c.a'));
+    assert(!hasValue(a, 'b.c.b'));
   });
 
   it('should work for booleans', function () {
-    hasValue(a, 'b.c.c').should.be.true;
-    hasValue(a, 'b.c.d').should.be.true;
+    assert(hasValue(a, 'b.c.c'));
+    assert(hasValue(a, 'b.c.d'));
   });
 
   it('shold work for strings', function () {
-    hasValue(a, 'b.c.e').should.be.false;
-    hasValue(a, 'b.c.f').should.be.true;
+    assert(!hasValue(a, 'b.c.e'));
+    assert(hasValue(a, 'b.c.f'));
   });
 
   it('should work for numbers', function () {
-    hasValue(a, 'b.c.g').should.be.true;
-    hasValue(a, 'b.c.h').should.be.true;
+    assert(hasValue(a, 'b.c.g'));
+    assert(hasValue(a, 'b.c.h'));
   });
 
   it('should treat zero as null when `noZero` is set', function () {
-    hasValue(a, 'b.c.i', true).should.be.false;
+    assert(!hasValue(a, 'b.c.i', true));
   });
 
   it('should work for objects', function () {
-    hasValue(a, 'b.c.j').should.be.false;
-    hasValue(a, 'b.c.k').should.be.true;
+    assert(!hasValue(a, 'b.c.j'));
+    assert(hasValue(a, 'b.c.k'));
   });
 
   it('should work for arrays', function () {
-    hasValue(a, 'b.c.l').should.be.false;
-    hasValue(a, 'b.c.m').should.be.true;
+    assert(!hasValue(a, 'b.c.l'));
+    assert(hasValue(a, 'b.c.m'));
   });
 
   it('should work for functions', function () {
-    hasValue(a, 'b.c.n').should.be.true;
-    hasValue(a, 'b.c.o').should.be.false;
-    hasValue(a, 'b.c.o').should.not.be.true;
+    assert(hasValue(a, 'b.c.n'));
+    assert(!hasValue(a, 'b.c.o'));
+    assert(!hasValue(a, 'b.c.o'));
   });
 });
 
 
 describe('single values', function () {
   it('should work for nulls', function () {
-    hasValue(null).should.be.false;
-    hasValue(undefined).should.be.false;
+    assert(!hasValue(null));
+    assert(!hasValue(undefined));
   });
 
   it('should work for booleans', function () {
-    hasValue(true).should.be.true;
-    hasValue(false).should.be.true;
+    assert(hasValue(true));
+    assert(hasValue(false));
   });
 
   it('shold work for strings', function () {
-    hasValue('').should.be.false;
-    hasValue('string').should.be.true;
+    assert(!hasValue(''));
+    assert(hasValue('string'));
   });
 
   it('should work for numbers', function () {
-    hasValue(0).should.be.true;
-    hasValue(1).should.be.true;
+    assert(hasValue(0));
+    assert(hasValue(1));
   });
 
   it('should treat zero as null when `noZero` is set', function () {
-    hasValue(0, true).should.be.false;
+    assert(!hasValue(0, true));
   });
 
   it('should work for objects', function () {
-    hasValue({}).should.be.false;
-    hasValue({a: 'b'}).should.be.true;
+    assert(!hasValue({}));
+    assert(hasValue({a: 'b'}));
   });
 
   it('should work for arrays', function () {
-    hasValue([]).should.be.false;
-    hasValue(['a', 'b']).should.be.true;
+    assert(!hasValue([]));
+    assert(hasValue(['a', 'b']));
   });
 
   it('should work for functions', function () {
-    hasValue(function (foo) {}).should.be.true;
-    hasValue(function () {}).should.be.false;
+    assert(hasValue(function (foo) {}));
+    assert(!hasValue(function () {}));
   });
 });
 
 
 describe('isEmpty', function () {
   it('should work for nulls', function () {
-    isEmpty(null).should.be.true;
-    isEmpty(undefined).should.be.true;
+    assert(isEmpty(null));
+    assert(isEmpty(undefined));
   });
 
   it('should work for booleans', function () {
-    isEmpty(false).should.be.false;
-    isEmpty(true).should.be.false;
+    assert(!isEmpty(false));
+    assert(!isEmpty(true));
   });
 
   it('shold work for strings', function () {
-    isEmpty('').should.be.true;
-    isEmpty('string').should.be.false;
+    assert(isEmpty(''));
+    assert(!isEmpty('string'));
   });
 
   it('should work for numbers', function () {
-    isEmpty(0).should.be.false;
-    isEmpty(1).should.be.false;
+    assert(!isEmpty(0));
+    assert(!isEmpty(1));
   });
 
   it('should treat zero as null when `noZero` is set', function () {
-    isEmpty(0, true).should.be.true;
+    assert(isEmpty(0, true));
   });
 
   it('should work for objects', function () {
-    isEmpty({}).should.be.true;
-    isEmpty({a: 'b'}).should.be.false;
+    assert(isEmpty({}));
+    assert(!isEmpty({a: 'b'}));
   });
 
   it('should work for arrays', function () {
-    isEmpty([]).should.be.true;
-    isEmpty(['a', 'b']).should.be.false;
+    assert(isEmpty([]));
+    assert(!isEmpty(['a', 'b']));
   });
 
   it('should work for functions', function () {
-    isEmpty(function (foo) {}).should.be.false;
-    isEmpty(function () {}).should.be.true;
+    assert(!isEmpty(function (foo) {}));
+    assert(isEmpty(function () {}));
   });
 });
