cabe6518c4ee3c5ce9e0709fc5ee144f4776c034
jonschlinkert <jon.schlinkert@sellside.com>
2018-01-30 20:05:10 -0500

    refactor
    - uses es6 code, so node engine is now >= 6.0
    - now returns false when the first argument is not an object, array or function, or the second argument is not a string or array

--- index.js
+++ index.js
@@ -11,5 +11,12 @@ const get = require('get-value');
 const has = require('has-values');
 
 module.exports = function(obj, path, options) {
-  return has(get(obj, path, options));
+  if (isObject(obj) && typeof path === 'string' || Array.isArray(path)) {
+    return has(get(obj, path, options));
+  }
+  return false;
 };
+
+function isObject(val) {
+  return val != null && typeof val === 'object' || typeof val === 'function' || Array.isArray(val);
+}
--- test.js
+++ test.js
@@ -5,81 +5,87 @@ const assert = require('assert');
 const has = require('./');
 
 describe('has value', function() {
+  it('should be false when the first argument is not an object', function() {
+    assert(!has(null));
+    assert(!has(undefined));
+    assert(!has(true));
+    assert(!has(false));
+    assert(!has(''));
+    assert(!has('foo'));
+    assert(!has(0));
+    assert(!has(9));
+  });
+
+  it('should be false when the second argument is not a string or array', function() {
+    assert(!has(['']));
+    assert(!has(['foo']));
+    assert(!has([0]));
+    assert(!has([[], []]));
+    assert(!has([[]]));
+    assert(!has([]));
+    assert(!has([null]));
+    assert(!has([undefined]));
+    assert(!has(function() {}));
+    assert(!has({ foo: '' }));
+    assert(!has({ foo: 0 }));
+    assert(!has({ foo: 1 }));
+    assert(!has({ foo: 9 }));
+    assert(!has({ foo: [[]] }));
+    assert(!has({ foo: [] }));
+    assert(!has({ foo: function() {} }));
+    assert(!has({ foo: null }));
+    assert(!has({ foo: undefined }));
+    assert(!has({ foo: { bar: 'baz' } }));
+    assert(!has({ foo: {} }));
+    assert(!has({}));
+  });
+
   it('should be true when property value is "null"', function() {
-    assert(has(null));
     assert(has({ foo: null }, 'foo'));
     assert(has({ foo: { bar: null } }, 'foo.bar'));
   });
 
   it('should be false when property value is "undefined"', function() {
-    assert(!has(undefined));
     assert(!has({ foo: undefined }, 'foo'));
     assert(!has({ foo: { bar: undefined } }, 'foo.bar'));
   });
 
   it('should be true when property value is boolean', function() {
-    assert(has(true));
-    assert(has(false));
     assert(has({ foo: true }, 'foo'));
     assert(has({ foo: false }, 'foo'));
     assert(has({ foo: { bar: false } }, 'foo.bar'));
   });
 
   it('should be false when property value is an empty string', function() {
-    assert(!has(''));
     assert(!has({ foo: '' }, 'foo'));
     assert(!has({ foo: { bar: '' } }, 'foo.bar'));
   });
 
   it('should be true when property value is a string', function() {
-    assert(has('foo'));
     assert(has({ foo: 'abc' }, 'foo'));
     assert(has({ foo: { bar: 'abc' } }, 'foo.bar'));
   });
 
   it('should be true for numbers', function() {
-    assert(has(0));
-    assert(has({ foo: 0 }));
     assert(has({ foo: 0 }, 'foo'));
     assert(has({ foo: { bar: 0 } }, 'foo.bar'));
-
-    assert(has(9));
-    assert(has({ foo: 9 }));
     assert(has({ foo: 9 }, 'foo'));
     assert(has({ foo: { bar: 9 } }, 'foo.bar'));
   });
 
   it('should work for objects', function() {
-    assert(has({ foo: 1 }));
-    assert(has({ foo: null }));
     assert(has({ foo: null }, 'foo'));
     assert(has({ foo: { bar: null } }, 'foo.bar'));
-    assert(has({ foo: { bar: 'baz' } }));
     assert(has({ foo: { bar: 'baz' } }, 'foo.bar'));
 
-    assert(!has({}));
-    assert(!has({ foo: '' }));
-    assert(!has({ foo: undefined }));
-    assert(!has({ foo: {} }));
     assert(!has({ foo: {} }, 'foo'));
     assert(!has({ foo: { bar: {} } }, 'foo.bar'));
   });
 
   it('should work for arrays', function() {
-    assert(!has([]));
-    assert(!has({ foo: [] }));
     assert(!has({ foo: [] }, 'foo'));
-    assert(!has(['']));
-    assert(!has([undefined]));
-    assert(!has([[]]));
-    assert(!has([[], []]));
-    assert(!has({ foo: [[]] }));
     assert(!has({ foo: [[], []] }, 'foo'));
     assert(!has({ foo: { bar: [[], []] } }, 'foo.bar'));
-
-    assert(has([0]));
-    assert(has([null]));
-    assert(has(['foo']));
     assert(has({ foo: [0] }, 'foo'));
     assert(has({ foo: [null] }, 'foo'));
     assert(has({ foo: ['foo'] }, 'foo'));
@@ -89,8 +95,6 @@ describe('has value', function() {
   });
 
   it('should work for functions', function() {
-    assert(has(function() {}));
-    assert(has({ foo: function() {} }));
     assert(has({ foo: function() {} }, 'foo'));
     assert(has({ foo: { bar: function() {} } }, 'foo.bar'));
   });
