6492419b113d12098d3f8b02ee376b0291960313
jonschlinkert <jon.schlinkert@sellside.com>
2018-01-30 20:06:35 -0500

    fix statement

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const get = require('get-value');
 const has = require('has-values');
 
 module.exports = function(obj, path, options) {
-  if (isObject(obj) && typeof path === 'string' || Array.isArray(path)) {
+  if (isObject(obj) && (typeof path === 'string' || Array.isArray(path))) {
     return has(get(obj, path, options));
   }
   return false;
